2f1497ef2f5135919937f3646c81cbb85e537ae4
atahanata <32178476+atahanata@users.noreply.github.com>
2020-12-31 15:32:14 +0300

    Update app.js

--- termProject/app.js
+++ termProject/app.js
@@ -1,25 +1,45 @@
 const addForm = document.querySelector(".add");
 const list = document.querySelector(".todos");
-
+let todoList = [];
 const generateTemplate = (todo) => {
-    const html = `
-      <li class="list-group-item d-flex justify-content-between align-items-center">
-          <span>${todo}</span>
-          <i class="far fa-trash-alt delete"></i>
-      </li>
-      `;
-  
-    list.innerHTML += html;
-  };
-  
+  const html = `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${todo}</span>
+        <i class="far fa-trash-alt delete"></i>
+    </li>
+    `;
+
+  list.innerHTML += html;
+};
+if (localStorage.getItem("todoList")) {
+  todoList = JSON.parse(localStorage.getItem("todoList"));
+  todoList.forEach(todo => {
+    generateTemplate(todo);
+  });
+}
+
 
 addForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const todo = addForm.add.value.trim();
-  
-    if (todo.length) {
-      generateTemplate(todo);
-      addForm.reset();
+  e.preventDefault();
+  const todo = addForm.add.value.trim();
+
+  if (todo.length) {
+    todoList.push(todo);
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+    generateTemplate(todo);
+    addForm.reset();
+  }
+});
+
+// Delete Todos
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    let value = e.target.parentElement.querySelector("span").innerHTML;
+    const index = todoList.indexOf(value);
+    if (index !== -1) {
+      todoList.splice(index, 1);
+      localStorage.setItem("todoList", JSON.stringify(todoList));
     }
-  });
-  
+    e.target.parentElement.remove();
+  }
+});
